f1e1d5c48007bd5c7fa5bd292e4aa824be553b11
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2026-09-14 13:32:49 +0700

    wrap home card indicators in a container div

--- index.php
+++ index.php
@@ -290,29 +290,31 @@ if (file_exists('/etc/os-release')) {
             <div class="card card-home">
                 <div class="card-home-left">
                     <h1 class="host-title"><?= htmlspecialchars($hostname) ?> - <?= htmlspecialchars($os_name) ?> <span class="dot"></span></h1>
-                    <div class="badge-ip">
-                        <svg class="icon-sm" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                        </svg>
-                        <span id="ip-display">Loading...</span>
-                    </div>
-                    <div class="uptime">
-                        <svg class="icon-xs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                        Uptime <span id="uptime-display">N/A</span>
-                    </div>
-                    <div class="uptime" style="margin-left: 10px; color: #10b981;">
-                        <svg class="icon-xs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-                        </svg>
-                        <span id="net-rx">0 B</span>
-                    </div>
-                    <div class="uptime" style="margin-left: 10px; color: #f43f5e;">
-                        <svg class="icon-xs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18" />
-                        </svg>
-                        <span id="net-tx">0 B</span>
+                    <div class="home-indicators">
+                        <div class="badge-ip">
+                            <svg class="icon-sm" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                            </svg>
+                            <span id="ip-display">Loading...</span>
+                        </div>
+                        <div class="uptime">
+                            <svg class="icon-xs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <span id="uptime-display">N/A</span>
+                        </div>
+                        <div class="uptime" style="color: #10b981;">
+                            <svg class="icon-xs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+                            </svg>
+                            <span id="net-rx">0 B</span>
+                        </div>
+                        <div class="uptime" style="color: #f43f5e;">
+                            <svg class="icon-xs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18" />
+                            </svg>
+                            <span id="net-tx">0 B</span>
+                        </div>
                     </div>
                 </div>
                 <div class="card-home-right">
